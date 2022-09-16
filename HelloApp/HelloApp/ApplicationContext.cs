372841dd38be145334c82ac94d8e4d3a11e2396b
ArtemStryzhakov <77277962+ArtemStryzhakov@users.noreply.github.com>
2022-09-16 11:30:03 +0300

    Add files via upload

---
+++ HelloApp/HelloApp/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApp
+{
+    public class ApplicationContext : DbContext // DbContext: определяет контекст данных, используемый для взаимодействия с базой данных
+    {
+        public DbSet<User> Users => Set<User>(); // DbSet/DbSet<TEntity>: представляет набор объектов, которые хранятся в базе данных
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) // DbContextOptionsBuilder: устанавливает параметры подключения
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+}
---
+++ HelloApp/HelloApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using HelloApp;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    // создаем два объекта User
+    User tom = new User { Name = "Tom", Age = 33, Gender = "Male" };
+    User alice = new User { Name = "Alice", Age = 26, Gender = "Female" };
+
+    // добавляем их в бд
+    db.Users.Add(tom);
+    db.Users.Add(alice);
+    db.SaveChanges();
+    Console.WriteLine("Elements were successfuly saved");
+
+    // получаем объекты из бд и выводим на консоль
+    var users = db.Users.ToList();
+    Console.WriteLine("Elements list:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age} - {u.Gender}");
+    }
+}
